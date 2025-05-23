94342cfae073eecc75677a5e03bf2d695294cace
ArtDaSak <182381184+ArtDaSak@users.noreply.github.com>
2025-05-23 12:47:08 -0500

    feat: :card_file_box: Add DML File

---
+++ DML.sql
@@ -0,0 +1,9 @@
+-- DML
+
+-- Insertar datos en la tabla empleados
+INSERT INTO empleados (id, nombre, apellido, correo) VALUES
+(1, 'Carlos', 'Ramírez', 'carlos.ramirez@email.com'),
+(2, 'Lucía', 'Gómez', 'lucia.gomez@email.com'),
+(3, 'Miguel', 'Fernández', 'miguel.fernandez@email.com'),
+(4, 'Ana', 'Martínez', 'ana.martinez@email.com'),
+(5, 'Javier', 'Pérez', 'javier.perez@email.com');
